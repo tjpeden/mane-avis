0cb667ef3ca159ccb03c5ad1a75103ef1b1e4fd3
TJ Peden <tj.peden@tj-coding.com>
2015-06-16 10:05:52 -0500

    Customized for my needs

--- StringStream/StringStream.h
+++ StringStream/StringStream.h
@@ -6,19 +6,18 @@
 class StringStream : public Stream
 {
 public:
-    StringStream(const String &s) : string(s), position(0) { }
+    StringStream(const String &s) : value(s), position(0) { }
 
     // Stream methods
-    virtual int available() { return string.length() - position; }
-    virtual int read() { return position < string.length() ? string[position++] : -1; }
-    virtual int peek() { return position < string.length() ? string[position] : -1; }
+    virtual int available() { return value.length() - position; }
+    virtual int read() { return position < value.length() ? value[position++] : -1; }
+    virtual int peek() { return position < value.length() ? value[position] : -1; }
     virtual void flush() { };
     // Print methods
-    virtual size_t write(uint8_t c) { string += (char)c; };
+    virtual size_t write(uint8_t c) { value += (char)c; };
 
-private:
-    String string;
-    int length;
+protected:
+    String value;
     int position;
 };
 
