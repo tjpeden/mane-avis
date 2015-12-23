8dedf2764c018c10070088db8a43bda1881f57cc
TJ Peden <tj.peden@tj-coding.com>
2015-12-23 17:46:45 -0600

    Convert tabs to spaces

--- Adafruit_mfGFX/fonts.h
+++ Adafruit_mfGFX/fonts.h
@@ -1,6 +1,6 @@
 //
 //  fonts.h
-//  
+//
 
 #ifndef _fonts_h
 #define _fonts_h
@@ -8,21 +8,21 @@
 #include "application.h"
 
 // Font selection descriptors - Add an entry for each new font and number sequentially
-#define TIMESNR_8	0
-#define CENTURY_8	1
-#define ARIAL_8		2
-#define COMICS_8	3
-#define GLCDFONT	4
-#define TEST		5
+#define TIMESNR_8  0
+#define CENTURY_8  1
+#define ARIAL_8    2
+#define COMICS_8   3
+#define GLCDFONT   4
+#define TEST       5
 
 #define FONT_START 0
-#define FONT_END 1
+#define FONT_END   1
 
 struct FontDescriptor
 {
-	uint8_t	width;		// width in bits
-	uint8_t	height; 	// char height in bits
-	uint16_t offset;	// offset of char into char array
+  uint8_t  width;    // width in bits
+  uint8_t  height;   // char height in bits
+  uint16_t offset;   // offset of char into char array
 };
 
 // Font references - add pair of references for each new font
