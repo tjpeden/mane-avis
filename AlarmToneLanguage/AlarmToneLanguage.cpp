3f24c0280e10636836747c482cd791d07d48f9a3
TJ Peden <tj.peden@tj-coding.com>
2016-01-08 15:11:22 -0600

    Add namespace

--- AlarmToneLanguage/AlarmToneLanguage.cpp
+++ AlarmToneLanguage/AlarmToneLanguage.cpp
@@ -2,139 +2,141 @@
 
 #include "AlarmToneLanguage.h"
 
-AlarmToneLanguage::AlarmToneLanguage(const String &value) : StringStream(value) {
-  duration = 0;
+namespace Alarming {
+  AlarmToneLanguage::AlarmToneLanguage(const String &value) : StringStream(value) {
+    duration = 0;
 
-  default_duration = 4;
-  default_octave   = 4;
-}
-
-AlarmToneLanguage::~AlarmToneLanguage() {}
-
-void AlarmToneLanguage::initialize() {
-  uint16_t bpm = 63;
-  String config;
-
-  name   = readStringUntil(':');
-  config = readStringUntil(':');
-
-  uint8_t index;
-  while(config.length() > 0) {
-    String option;
-
-    index = config.indexOf(',');
-
-    if(index >= 0) {
-      option = config.substring(0, index);
-      config = config.substring(index + 1);
-    } else {
-      option = String(config);
-      config = String();
-    }
-
-    switch(option.charAt(0)) {
-      case 'd':
-        default_duration = option.substring(2).toInt();
-        break;
-      case 'o':
-        default_octave   = option.substring(2).toInt();
-        break;
-      case 'b':
-        bpm              = option.substring(2).toInt();
-    }
+    default_duration = 4;
+    default_octave   = 4;
   }
 
-  wholenote = (60 * 1000 / (float)bpm) * 4;
+  AlarmToneLanguage::~AlarmToneLanguage() {}
 
-  first_note = position;
-}
+  void AlarmToneLanguage::initialize() {
+    uint16_t bpm = 63;
+    String config;
 
-void AlarmToneLanguage::rewind() {
-  position = first_note;
-  duration = 100;
-}
+    name   = readStringUntil(':');
+    config = readStringUntil(':');
 
-bool AlarmToneLanguage::next() {
-  if(available()) {
-    uint8_t sharp;
+    uint8_t index;
+    while(config.length() > 0) {
+      String option;
 
-    if(isdigit((char)peek())) {
-      duration = wholenote / parseInt();
-    } else {
-      duration = wholenote / default_duration;
+      index = config.indexOf(',');
+
+      if(index >= 0) {
+        option = config.substring(0, index);
+        config = config.substring(index + 1);
+      } else {
+        option = String(config);
+        config = String();
+      }
+
+      switch(option.charAt(0)) {
+        case 'd':
+          default_duration = option.substring(2).toInt();
+          break;
+        case 'o':
+          default_octave   = option.substring(2).toInt();
+          break;
+        case 'b':
+          bpm              = option.substring(2).toInt();
+      }
     }
 
-    switch((char)read()) {
-      case 'c': note = NOTE_C; sharp =  8; break;
-      case 'd': note = NOTE_D; sharp =  9; break;
-      case 'e': note = NOTE_E; sharp =  0; break;
-      case 'f': note = NOTE_F; sharp = 10; break;
-      case 'g': note = NOTE_G; sharp = 12; break;
-      case 'a': note = NOTE_A; sharp = 13; break;
-      case 'b': note = NOTE_B; sharp =  0; break;
-      case 'p': note = NOTE_P; sharp =  0; break;
-    }
+    wholenote = (60 * 1000 / (float)bpm) * 4;
 
-    if((char)peek() == '#') {
-      note += sharp;
-      read();
-    }
-
-    if((char)peek()== '.') {
-      duration += duration / 2;
-      read();
-    }
-
-    if(isdigit((char)peek())) {
-      octave = parseInt();
-    } else {
-      octave = default_octave;
-    }
-
-    if((char)peek() == ',') read();
-
-    return true;
+    first_note = position;
   }
 
-  return false;
-}
-
-bool AlarmToneLanguage::isRest() const {
-  return note == NOTE_P;
-}
-
-uint32_t AlarmToneLanguage::getNote() const {
-  return note * pow(2, octave - min_octave);
-}
-
-uint32_t AlarmToneLanguage::getDuration() const {
-  return duration;
-}
-
-size_t AlarmToneLanguage::printTo(Print &p) const {
-  size_t length = 0;
-  String _note;
-
-  switch(note) {
-    case NOTE_P: _note = "Rest"; break;
-    case NOTE_C: _note = "C   "; break;
-    case NOTE_D: _note = "D   "; break;
-    case NOTE_E: _note = "E   "; break;
-    case NOTE_F: _note = "F   "; break;
-    case NOTE_G: _note = "G   "; break;
-    case NOTE_A: _note = "A   "; break;
-    case NOTE_B: _note = "B   "; break;
-    case NOTE_C +  8: _note = "C#  "; break;
-    case NOTE_D +  9: _note = "D#  "; break;
-    case NOTE_F + 10: _note = "F#  "; break;
-    case NOTE_G + 12: _note = "G#  "; break;
-    case NOTE_A + 13: _note = "A#  "; break;
+  void AlarmToneLanguage::rewind() {
+    position = first_note;
+    duration = 100;
   }
 
-  length += p.print("Octave: " + String(octave) + " ");
-  length += p.print("Note: " + _note + " => ");
-  length += p.print(String(getNote()) + " ");
-  length += p.println("Duration: " + String(duration));
+  bool AlarmToneLanguage::next() {
+    if(available()) {
+      uint8_t sharp;
 
-  return length;
+      if(isdigit((char)peek())) {
+        duration = wholenote / parseInt();
+      } else {
+        duration = wholenote / default_duration;
+      }
+
+      switch((char)read()) {
+        case 'c': note = NOTE_C; sharp =  8; break;
+        case 'd': note = NOTE_D; sharp =  9; break;
+        case 'e': note = NOTE_E; sharp =  0; break;
+        case 'f': note = NOTE_F; sharp = 10; break;
+        case 'g': note = NOTE_G; sharp = 12; break;
+        case 'a': note = NOTE_A; sharp = 13; break;
+        case 'b': note = NOTE_B; sharp =  0; break;
+        case 'p': note = NOTE_P; sharp =  0; break;
+      }
+
+      if((char)peek() == '#') {
+        note += sharp;
+        read();
+      }
+
+      if((char)peek()== '.') {
+        duration += duration / 2;
+        read();
+      }
+
+      if(isdigit((char)peek())) {
+        octave = parseInt();
+      } else {
+        octave = default_octave;
+      }
+
+      if((char)peek() == ',') read();
+
+      return true;
+    }
+
+    return false;
+  }
+
+  bool AlarmToneLanguage::isRest() const {
+    return note == NOTE_P;
+  }
+
+  uint32_t AlarmToneLanguage::getNote() const {
+    return note * pow(2, octave - min_octave);
+  }
+
+  uint32_t AlarmToneLanguage::getDuration() const {
+    return duration;
+  }
+
+  size_t AlarmToneLanguage::printTo(Print &p) const {
+    size_t length = 0;
+    String _note;
+
+    switch(note) {
+      case NOTE_P: _note = "Rest"; break;
+      case NOTE_C: _note = "C   "; break;
+      case NOTE_D: _note = "D   "; break;
+      case NOTE_E: _note = "E   "; break;
+      case NOTE_F: _note = "F   "; break;
+      case NOTE_G: _note = "G   "; break;
+      case NOTE_A: _note = "A   "; break;
+      case NOTE_B: _note = "B   "; break;
+      case NOTE_C +  8: _note = "C#  "; break;
+      case NOTE_D +  9: _note = "D#  "; break;
+      case NOTE_F + 10: _note = "F#  "; break;
+      case NOTE_G + 12: _note = "G#  "; break;
+      case NOTE_A + 13: _note = "A#  "; break;
+    }
+
+    length += p.print("Octave: " + String(octave) + " ");
+    length += p.print("Note: " + _note + " => ");
+    length += p.print(String(getNote()) + " ");
+    length += p.println("Duration: " + String(duration));
+
+    return length;
+  }
 }
--- AlarmToneLanguage/AlarmToneLanguage.h
+++ AlarmToneLanguage/AlarmToneLanguage.h
@@ -5,43 +5,45 @@
 
 #include "StringStream.h"
 
-class AlarmToneLanguage : private StringStream, public Printable {
-  const uint8_t min_octave = 3;
+namespace Alarming {
+  class AlarmToneLanguage : private StringStream, public Printable {
+    const uint8_t min_octave = 3;
 
-  enum {
-    NOTE_P = 0,
-    NOTE_C = 131,
-    NOTE_D = 147,
-    NOTE_E = 165,
-    NOTE_F = 175,
-    NOTE_G = 196,
-    NOTE_A = 220,
-    NOTE_B = 247,
+    enum {
+      NOTE_P = 0,
+      NOTE_C = 131,
+      NOTE_D = 147,
+      NOTE_E = 165,
+      NOTE_F = 175,
+      NOTE_G = 196,
+      NOTE_A = 220,
+      NOTE_B = 247,
+    };
+
+    uint8_t default_duration;
+    uint8_t default_octave;
+
+    uint8_t octave;
+    uint16_t note;
+    uint32_t duration;
+    uint32_t wholenote;
+
+    uint32_t first_note;
+
+    String name;
+  public:
+    AlarmToneLanguage(const String&);
+    ~AlarmToneLanguage();
+
+    void initialize();
+    void rewind();
+    bool next();
+    bool isRest() const;
+    uint32_t getNote() const;
+    uint32_t getDuration() const;
+
+    virtual size_t printTo(Print&) const;
   };
 
-  uint8_t default_duration;
-  uint8_t default_octave;
-
-  uint8_t octave;
-  uint16_t note;
-  uint32_t duration;
-  uint32_t wholenote;
-
-  uint32_t first_note;
-
-  String name;
-public:
-  AlarmToneLanguage(const String&);
-  ~AlarmToneLanguage();
-
-  void initialize();
-  void rewind();
-  bool next();
-  bool isRest() const;
-  uint32_t getNote() const;
-  uint32_t getDuration() const;
-
-  virtual size_t printTo(Print&) const;
-};
-
-#endif // _ALARM_TONE_LANGUAGE_H
+  #endif // _ALARM_TONE_LANGUAGE_H
+}
